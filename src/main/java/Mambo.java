1f8c16dc2697134e1f9307dac0c83e51badb6422
kentalim2 <kentalim2@gmail.com>
2025-08-21 13:07:32 +0800

    Rename Duke to Mambo and change code

---
+++ src/main/java/Mambo.java
@@ -0,0 +1,11 @@
+public class Mambo {
+    public static void main(String[] args) {
+        String entry = "------------------------------------------------------\n"
+                        + "Hi! I'm Mambo, your personal assistant and chatbot!\n"
+                        + "What can I do for you today? ei, ei Mun!";
+        String exit = "Byee, see you again!";
+
+        System.out.println(entry);
+        System.out.println(exit);
+    }
+}
